05ec0ba990d030ff21e59b3d6c4cc583d5315c01
Masayoshi Tokumoto <39050027+masayoshi-toku@users.noreply.github.com>
2022-12-30 12:05:30 +0900

    feat: 成果物をコミット

---
+++ fib.py
@@ -0,0 +1,16 @@
+import pyo3_example
+from time import time
+
+
+def fib(a):
+    if a <= 2:
+        return 1
+    return fib(a - 2) + fib(a - 1)
+
+start = time()
+_ = fib(50)
+print("python: ", time() - start, "sec")
+
+start = time()
+_ = pyo3_example.fib(50)
+print("rust  : ", time() - start, "sec")
